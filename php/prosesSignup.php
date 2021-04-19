2ebca7e214d6f95fa1522928a052839523ac6ccc
Muhammad Ikhsan Ananda <80374569+ikhsanananda@users.noreply.github.com>
2021-04-19 10:55:28 +0700

    Add files via upload

---
+++ php/prosesSignup.php
@@ -0,0 +1,29 @@
+<?php
+include("config.php");
+//cek apakah tombol udah di klik
+if(isset($_POST['daftar']))
+{
+    //ambil data dari formulir
+    $username=$_POST['username'];
+    $email=$_POST['email'];
+    $pass=$_POST['passwordorg'];
+    $cpass=$_POST['cpassword'];
+
+    //buat query
+    $query=pg_query("INSERT INTO daftar_user(username, email, passwordorg, cpassword) VALUEs('$username', '$email', '$pass', '$cpass')");
+
+    //apakah berhasil
+    if( $query==TRUE)
+    {
+        header('Location:FavouriteNews.html');
+    }
+    else
+    {
+        header('Location:SignupLogin.php');
+    }
+}
+else 
+{
+    die("Akses gagal...");
+}
+?>
