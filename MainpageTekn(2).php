e702d7394e5005aa456ebdf2a2380101d1ec15bc
Muhammad Ikhsan Ananda <80374569+ikhsanananda@users.noreply.github.com>
2021-05-20 22:59:02 +0700

    Add files via upload

---
+++ MainpageTekn(2).php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Wisata Luar Angkasa Gratis lewat Google Chrome, Begini Caranya</title>
+
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="Main Page/css/all.css">
+    <link rel="stylesheet" href="Main Page/css/fonts.css">
+
+    <!-- --------- Owl-Carousel ------------------->
+    <link rel="stylesheet" href="Main Page/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="Main Page/css/owl.theme.default.min.css">
+
+    <!-- ------------ AOS Library ------------------------- -->
+    <link rel="stylesheet" href="Main Page/css/aos.css">
+
+    <!-- Custom Style   -->
+    <link rel="stylesheet" href="Main Page/css/Style.css">
+</head>
+
+<body>
+
+    <!-- ----------------------------  Navigation ---------------------------------------------- -->
+
+    <nav class="nav">
+        <div class="nav-menu flex-row">
+            <div class="nav-brand">
+                <a href="#" class="text-gray">RPL News</a>
+            </div>
+            <div class="toggle-collapse">
+                <div class="toggle-icons">
+                    <i class="fas fa-bars"></i>
+                </div>
+            </div>
+            <div>
+                <ul class="nav-items">
+                    <li class="nav-link">
+                        <a href="#">News Update</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">Share The Story</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="social text-gray">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+            </div>
+        </div>
+    </nav>
+
+    <!-- ------------x---------------  Navigation --------------------------x------------------- -->
+
+    <!----------------------------- Main Site Section ------------------------------>
+
+    <main>
+        <!-- ---------------------- Site Content -------------------------->
+
+        <section class="container">
+            <div class="site-content">
+                <div class="posts">
+                    <div class="post-content" data-aos="zoom-in" data-aos-delay="200">
+                        <div class="post-image">
+                            <div>
+                                <img src="Main Page/assets/Tekn3angk.png" class="img" alt="blog1">
+                            </div>
+                            <div class="post-info flex-row">
+                                <span><i class="fas fa-user text-gray"></i>&nbsp;&nbsp;Admin</span>
+                                <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;January 14, 2019</span>
+                                <span>
+                                <a href="ceklogin.php">
+                                        <button>
+                                                <i class="far fa-heart text-gray"></i>
+                                            </button>&nbsp;&nbsp;Like
+                                            <?php
+                                                include("config.php");
+                                                session_start();
+                                                $nama_user=$_SESSION['username'];
+                                                $page=basename($_SERVER['PHP_SELF']);
+                                                $query=pg_query("INSERT INTO simpan_berita(username, nama_halaman) VALUEs('$nama_user', '$page')");
+                                            ?>
+                                    </a>
+                                </span>
+
+                            </div>
+                        </div>
+
+                        <div class="post-title">
+                            <a href="MainpageTekn(2).html">Wisata Luar Angkasa Gratis lewat Google Chrome, Begini Caranya
+                            </a>
+                            <p>Perusahaan roket milik miliarder Jeff Bezos, Blue Origin, tengah bersiap membawa enam turis untuk berwisata ke luar angkasa, pada Juli mendatang.
+                            </p>
+                            <p>Menurut kabar yang beredar, para turis perlu membayar Rp 2,8 triliun sebagai ongkos dari perjalanan wisata luar angkasa itu. Dengan biaya yang tinggi, sudah dipastikan tidak semua orang bisa menikmati wisata luar angkasa itu.
+                            </p>
+                            <p>Namun, ada cara lain yang bisa dilakukan bila kalian ingin menjelajahi ruang angkasa secara mudah dan pastinya gratis, yakni dengan mengunjungi laman eksperimental di Google Chrome bernama "100.000 Stars".
+                            </p>
+                            <p>Melalui laman tersebut, Google akan mengajak pengguna menjelajahi tata surya dan melihat 100.000 bintang yang jaraknya dekat dengan matahari. Google memvisualisasikan dan mendokumentasikan nama-nama 100.000 bintang tersebut.
+                            </p>
+                            <p>Untuk bisa menjelajahi tata surya dan melihat 100.000 bintang di galaksi, pengguna cukup masuk ke halaman stars.chromeexperiments.com atau melalui tautan berikut.
+                            </p>
+                            <p>Setelah itu, pengguna hanya perlu mengklik ikon play atau opsi "take a tour" di pojok kiri atas halaman, untuk mulai menjelajahi tata surya dan melihat 100.000 bintang yang telah berhasil didokumentasikan oleh Google.
+                            </p>
+                            <p>Pengguna akan dibawa mendekati pusat tata surya, yaitu matahari. Kemudian, pengguna akan perlahan diperlihatkan empat planet yang paling dekat dengan matahari.
+                            </p>
+                            <p>Lalu Google akan membawa kita semakin jauh dari matahari dan menampakkan beberapa bintang yang paling dekat dengan tata surya, di antaranya ada bintang bernama Altair, Kruger 60, Bernard's Star, Sirius, Proxima Centauri, dan
+                                sebagainya.
+                            </p>
+                            <div>
+                                <img src="Main Page/assets/angkasa1.png" class="img" alt="blog1">
+                            </div>
+                            <p>Bila sudah selesai, pengguna bisa dengan mudah mengeksplorasi lebih jauh soal tata surya dan bintang-bintang yang ada. Caranya cukup dengan melakukan zoom-in dan zoom-out menggunakan mouse atau trackpad.
+                            </p>
+                            <p>Misalnya, dengan melakukan zoom-in, pengguna bisa melihat nama-nama bintang dengan jelas. Lalu nama bintang itu juga bisa diklik dan akan menampilkan visual dan informasi singkat dari bintang tersebut.
+                            </p>
+                            <p>Foto dan data yang ditampilkan dalam laman eksperimen ini diambil dari berbagai sumber, termasuk dari Badan Antariksa Amerika Serikat (NASA) dan Badan Antariksa Eropa (ESA).
+                            </p>
+                            <p>Chrome Experiment ini memanfaatkan dukungan Google Chrome untuk WebGL, CSS3D, dan Web Audio.
+                            </p>
+                            <p>Musik latarnya diciptakan oleh SamHulick, komposer video game petualangan luar angkasa Mass Effect, sebagaimana dihimpun KompasTekno dari Open Culture, Minggu (16/5/2021).
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <aside class="sidebar">
+                    <div class="category">
+                        <h2>Category</h2>
+                        <ul class="category-list">
+                            <li class="list-items" data-aos="fade-left" data-aos-delay="100">
+                                <a href="#">Health</a>
+
+                            </li>
+                            <li class="list-items" data-aos="fade-left" data-aos-delay="200">
+                                <a href="#">Technology</a>
+
+                            </li>
+                            <li class="list-items" data-aos="fade-left" data-aos-delay="300">
+                                <a href="#">Lifestyle</a>
+
+                            </li>
+                            <li class="list-items" data-aos="fade-left" data-aos-delay="400">
+                                <a href="#">Shopping</a>
+
+                            </li>
+                            <li class="list-items" data-aos="fade-left" data-aos-delay="500">
+                                <a href="#">Food</a>
+
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="popular-post">
+                        <h2>Popular Post</h2>
+                        <div class="post-content" data-aos="flip-up" data-aos-delay="200">
+                            <div class="post-image">
+                                <div>
+                                    <img src="Main Page/assets/Tekn1redmi.jpg" class="img" alt="blog1">
+                                </div>
+                                <div class="post-info flex-row">
+                                    <span>
+                                        <i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 16,
+                                        2021
+                                    </span>
+                                    <span><button><a href="ceklogin.php"><i class="far fa-heart text-gray"></i></a></button>&nbsp;&nbsp;Like</span>
+                                </div>
+                            </div>
+
+                            <div class="post-title">
+                                <a href="MainpageTekn.html">Bocoran Spesifikasi Redmi Note 10s, Meluncur di Indonesia Besok
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="post-content" data-aos="flip-up" data-aos-delay="300">
+                            <div class="post-image">
+                                <div>
+                                    <img src="Main Page/assets/Tekn2tele.jpg" class="img" alt="blog1">
+                                </div>
+                                <div class="post-info flex-row">
+                                    <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 16,
+                                        2021</span>
+                                    <span><button><a href="ceklogin.php"><i class="far fa-heart text-gray"></i></a></button>&nbsp;&nbsp;Like</span>
+                                </div>
+                            </div>
+                            <div class="post-title">
+                                <a href="MainpageTekn(1).html">Telegram Ajak Pengguna WhatsApp Hapus Aplikasi</a>
+                            </div>
+                        </div>
+                        <div class="post-content" data-aos="flip-up" data-aos-delay="400">
+                            <div class="post-image">
+                                <div>
+                                    <img src="Main Page/assets/goto1.png" class="img" alt="blog1">
+                                </div>
+                                <div class="post-info flex-row">
+                                    <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 17,
+                                        2021</span>
+                                    <span><button><a href="ceklogin.php"><i class="far fa-heart text-gray"></i></a></button>&nbsp;&nbsp;Like</span>
+                                </div>
+                            </div>
+                            <div class="post-title">
+                                <a href="MainpageTekn(3).html">Gojek dan Tokopedia Resmi Merger Menjadi GoTo</a>
+                            </div>
+                        </div>
+
+                        <div class="post-content" data-aos="flip-up" data-aos-delay="500">
+                            <div class="post-image">
+                                <div>
+                                    <img src="Main Page/assets/wa1.jpg" class="img" alt="blog1">
+                                </div>
+                                <div class="post-info flex-row">
+                                    <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 18,
+                                        2021</span>
+                                    <span><button><a href="ceklogin.php"><i class="far fa-heart text-gray"></i></a></button>&nbsp;&nbsp;Like</span>
+                                </div>
+                            </div>
+                            <div class="post-title">
+                                <a href="MainpageTekn(4).html">WhatsApp Siapkan "Mode Menghilang", Seperti Apa?</a>
+                            </div>
+                        </div>
+
+                        <div class="post-content" data-aos="flip-up" data-aos-delay="600">
+                            <div class="post-image">
+                                <div>
+                                    <img src="Main Page/assets/popular-post/m-blog-5.jpg" class="img" alt="blog1">
+                                </div>
+                                <div class="post-info flex-row">
+                                    <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 17,
+                                        2021</span>
+                                    <span><button><a href="ceklogin.php"><i class="far fa-heart text-gray"></i></a></button>&nbsp;&nbsp;Like</span>
+                                </div>
+                            </div>
+                            <div class="post-title">
+                                <a href="MainpageTekn(5).html">Layanan 5G Telkomsel Meluncur Sebentar Lagi?</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="newsletter" data-aos="fade-up" data-aos-delay="300">
+                        <h2>Newsletter</h2>
+                        <div class="form-element">
+                            <input type="text" class="input-element" placeholder="Email">
+                            <button class="btn form-btn">Subscribe</button>
+                        </div>
+                    </div>
+                    <div class="popular-tags">
+                        <h2>Popular Tags</h2>
+                        <div class="tags flex-row">
+                            <span class="tag" data-aos="flip-up" data-aos-delay="100">Software</span>
+                            <span class="tag" data-aos="flip-up" data-aos-delay="200">technology</span>
+                            <span class="tag" data-aos="flip-up" data-aos-delay="300">travel</span>
+                            <span class="tag" data-aos="flip-up" data-aos-delay="400">illustration</span>
+                            <span class="tag" data-aos="flip-up" data-aos-delay="500">design</span>
+                            <span class="tag" data-aos="flip-up" data-aos-delay="600">lifestyle</span>
+                            <span class="tag" data-aos="flip-up" data-aos-delay="700">love</span>
+                            <span class="tag" data-aos="flip-up" data-aos-delay="800">project</span>
+                        </div>
+                    </div>
+                </aside>
+            </div>
+        </section>
+
+        <!-- -----------x---------- Site Content -------------x------------>
+
+    </main>
+
+    <!---------------x------------- Main Site Section ---------------x-------------->
+
+
+    <!-- --------------------------- Footer ---------------------------------------- -->
+
+    <footer class="footer">
+        <div class="container">
+            <div class="about-us" data-aos="fade-right" data-aos-delay="200">
+                <h2>About us</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quia atque nemo ad modi officiis iure, autem nulla tenetur repellendus.</p>
+            </div>
+            <div class="newsletter" data-aos="fade-right" data-aos-delay="200">
+                <h2>Newsletter</h2>
+                <p>Stay update with our latest</p>
+                <div class="form-element">
+                    <input type="text" placeholder="Email"><span><i class="fas fa-chevron-right"></i></span>
+                </div>
+            </div>
+            <div class="instagram" data-aos="fade-left" data-aos-delay="200">
+                <h2>Instagram</h2>
+                <div class="flex-row">
+                    <img src="Main Page/assets/instagram/thumb-card3.png" alt="insta1">
+                    <img src="Main Page/assets/instagram/thumb-card4.png" alt="insta2">
+                    <img src="Main Page/assets/instagram/thumb-card5.png" alt="insta3">
+                </div>
+                <div class="flex-row">
+                    <img src="Main Page/assets/instagram/thumb-card6.png" alt="insta4">
+                    <img src="Main Page/assets/instagram/thumb-card7.png" alt="insta5">
+                    <img src="Main Page/assets/instagram/thumb-card8.png" alt="insta6">
+                </div>
+            </div>
+            <div class="follow" data-aos="fade-left" data-aos-delay="200">
+                <h2>Follow us</h2>
+                <p>Let us be Social</p>
+                <div>
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-twitter"></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-youtube"></i>
+                </div>
+            </div>
+        </div>
+        <div class="rights flex-row">
+            <h4 class="text-gray">
+                Copyright ©2021 All rights reserved | made by RPL Teams
+            </h4>
+        </div>
+        <div class="move-up">
+            <span><i class="fas fa-arrow-circle-up fa-2x"></i></span>
+        </div>
+    </footer>
+
+    <!-- -------------x------------- Footer --------------------x------------------- -->
+
+    <!-- Jquery Library file -->
+    <script src="Main Page/js/Jquery3.4.1.min.js"></script>
+
+    <!-- --------- Owl-Carousel js ------------------->
+    <script src="Main Page/js/owl.carousel.min.js"></script>
+
+    <!-- ------------ AOS js Library  ------------------------- -->
+    <script src="Main Page/js/aos.js"></script>
+
+    <!-- Custom Javascript file -->
+    <script src="Main Page/js/main.js"></script>
+</body>
+
+</html>
