c736c7120b97c3532fc8bb3c875020cff448ed23
Muhammad Ikhsan Ananda <80374569+ikhsanananda@users.noreply.github.com>
2021-05-21 10:47:17 +0700

    Update MainpageTekn(3).php

--- MainpageTekn(3).php
+++ MainpageTekn(3).php
@@ -86,7 +86,14 @@
                                                 session_start();
                                                 $nama_user=$_SESSION['username'];
                                                 $page=basename($_SERVER['PHP_SELF']);
-                                                $query=pg_query("INSERT INTO simpan_berita(username, nama_halaman) VALUEs('$nama_user', '$page')");
+                                                
+                                                $pgsql=pg_query("SELECT count(nama_halaman) FROM simpan_berita WHERE nama_halaman='$page' AND username='$nama_user'");
+                                            
+                                                $val = pg_fetch_result($pgsql, 0, 0);
+                                                if($val=='0')
+                                                {
+                                                    $query=pg_query("INSERT INTO simpan_berita(username, nama_halaman) VALUEs('$nama_user', '$page')");
+                                                }
                                             ?>
                                     </a>
                                 </span>
@@ -95,7 +102,7 @@
                         </div>
 
                         <div class="post-title">
-                            <a href="MainpageTekn(3).html">Gojek dan Tokopedia Resmi Merger Menjadi GoTo
+                            <a href="MainpageTekn(3).php">Gojek dan Tokopedia Resmi Merger Menjadi GoTo
                             </a>
                             <p>Lama ditunggu, Gojek dan Tokopedia resmi bergabung di bawah payung grup GoTo. Hal tersebut diungkapkan langsung oleh CEO Tokopedia William Tanuwijaya dalam sebuah posting di akun Facebook miliknya.
                             </p>
