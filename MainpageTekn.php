dec9754262ec5db181f6d0e4cd2e6fe22d3cc93a
asmiazizah <80374921+asmiazizah@users.noreply.github.com>
2021-05-21 17:12:40 +0700

    Update MainpageTekn.php

--- MainpageTekn.php
+++ MainpageTekn.php
@@ -75,7 +75,7 @@
                             </div>
                             <div class="post-info flex-row">
                                 <span><i class="fas fa-user text-gray"></i>&nbsp;&nbsp;Admin</span>
-                                <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;January 14, 2019</span>
+                                <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 17, 2021</span>
                                 <span>
                                     <a href="ceklogin.php">
                                         <button>
@@ -234,7 +234,7 @@
                         <div class="post-content" data-aos="flip-up" data-aos-delay="600">
                             <div class="post-image">
                                 <div>
-                                    <img src="Main Page/assets/popular-post/m-blog-5.jpg" class="img" alt="blog1">
+                                    <img src="Main Page/assets/5G1.jpg" class="img" alt="blog1">
                                 </div>
                                 <div class="post-info flex-row">
                                     <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 17,
