447862a467772f3e0f9638ca9cbbded7bf77563a
asmiazizah <80374921+asmiazizah@users.noreply.github.com>
2021-05-21 17:31:58 +0700

    Update MainpageTekn(4).php

--- MainpageTekn(4).php
+++ MainpageTekn(4).php
@@ -171,7 +171,7 @@
                         <div class="post-content" data-aos="flip-up" data-aos-delay="200">
                             <div class="post-image">
                                 <div>
-                                    <img src="Main Page/assets/Tekn1redmi.jpg" class="img" alt="blog1">
+                                    <img src="Main Page/assets/Tekn2tele.jpg" class="img" alt="blog1">
                                 </div>
                                 <div class="post-info flex-row">
                                     <span>
@@ -183,15 +183,14 @@
                             </div>
 
                             <div class="post-title">
-                                <a href="MainpageTekn.html">Bocoran Spesifikasi Redmi Note 10s, Meluncur di Indonesia Besok
-                                </a>
+                                <a href="MainpageTekn(1).html">Telegram Ajak Pengguna WhatsApp Hapus Aplikasi</a>
                             </div>
                         </div>
 
                         <div class="post-content" data-aos="flip-up" data-aos-delay="300">
                             <div class="post-image">
                                 <div>
-                                    <img src="Main Page/assets/Tekn2tele.jpg" class="img" alt="blog1">
+                                    <img src="Main Page/assets/Tekn3angk.png" class="img" alt="blog1">
                                 </div>
                                 <div class="post-info flex-row">
                                     <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 16,
@@ -200,26 +199,10 @@
                                 </div>
                             </div>
                             <div class="post-title">
-                                <a href="MainpageTekn(1).html">Telegram Ajak Pengguna WhatsApp Hapus Aplikasi</a>
-                            </div>
-                        </div>
-                        <div class="post-content" data-aos="flip-up" data-aos-delay="400">
-                            <div class="post-image">
-                                <div>
-                                    <img src="Main Page/assets/Tekn3angk.png" class="img" alt="blog1">
-                                </div>
-                                <div class="post-info flex-row">
-                                    <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 17,
-                                        2021</span>
-                                    <span><button><a href="ceklogin.php"><i class="far fa-heart text-gray"></i></a></button>&nbsp;&nbsp;Like</span>
-                                </div>
-                            </div>
-                            <div class="post-title">
                                 <a href="MainpageTekn(2).html">Wisata Luar Angkasa Gratis lewat Google Chrome, Begini Caranya</a>
                             </div>
                         </div>
-
-                        <div class="post-content" data-aos="flip-up" data-aos-delay="500">
+                        <div class="post-content" data-aos="flip-up" data-aos-delay="400">
                             <div class="post-image">
                                 <div>
                                     <img src="Main Page/assets/goto1.png" class="img" alt="blog1">
@@ -235,10 +218,26 @@
                             </div>
                         </div>
 
+                        <div class="post-content" data-aos="flip-up" data-aos-delay="500">
+                            <div class="post-image">
+                                <div>
+                                    <img src="Main Page/assets/wa1.jpg" class="img" alt="blog1">
+                                </div>
+                                <div class="post-info flex-row">
+                                    <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 18,
+                                        2021</span>
+                                    <span><button><a href="ceklogin.php"><i class="far fa-heart text-gray"></i></a></button>&nbsp;&nbsp;Like</span>
+                                </div>
+                            </div>
+                            <div class="post-title">
+                                <a href="MainpageTekn(4).html">WhatsApp Siapkan "Mode Menghilang", Seperti Apa?</a>
+                            </div>
+                        </div>
+
                         <div class="post-content" data-aos="flip-up" data-aos-delay="600">
                             <div class="post-image">
                                 <div>
-                                    <img src="Main Page/assets/popular-post/m-blog-5.jpg" class="img" alt="blog1">
+                                    <img src="Main Page/assets/5G1.jpg" class="img" alt="blog1">
                                 </div>
                                 <div class="post-info flex-row">
                                     <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 17,
