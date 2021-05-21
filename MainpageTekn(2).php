9e5607a13e73ae77b19dd55787243c017593eba9
asmiazizah <80374921+asmiazizah@users.noreply.github.com>
2021-05-21 17:21:31 +0700

    Update MainpageTekn(2).php

--- MainpageTekn(2).php
+++ MainpageTekn(2).php
@@ -75,7 +75,7 @@
                             </div>
                             <div class="post-info flex-row">
                                 <span><i class="fas fa-user text-gray"></i>&nbsp;&nbsp;Admin</span>
-                                <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;January 14, 2019</span>
+                                <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 16, 2021</span>
                                 <span>
                                 <a href="ceklogin.php">
                                         <button>
@@ -169,7 +169,7 @@
                         <div class="post-content" data-aos="flip-up" data-aos-delay="200">
                             <div class="post-image">
                                 <div>
-                                    <img src="Main Page/assets/Tekn1redmi.jpg" class="img" alt="blog1">
+                                    <img src="Main Page/assets/Tekn2tele.jpg" class="img" alt="blog1">
                                 </div>
                                 <div class="post-info flex-row">
                                     <span>
@@ -181,8 +181,7 @@
                             </div>
 
                             <div class="post-title">
-                                <a href="MainpageTekn.html">Bocoran Spesifikasi Redmi Note 10s, Meluncur di Indonesia Besok
-                                </a>
+                                <a href="MainpageTekn(1).html">Telegram Ajak Pengguna WhatsApp Hapus Aplikasi</a>
                             </div>
                         </div>
 
