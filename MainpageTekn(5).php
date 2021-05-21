623a137d6ef687af7afe9cacc1489ab1ed023e2a
asmiazizah <80374921+asmiazizah@users.noreply.github.com>
2021-05-21 17:58:29 +0700

    Update MainpageTekn(5).php

--- MainpageTekn(5).php
+++ MainpageTekn(5).php
@@ -191,7 +191,7 @@
                             </div>
 
                             <div class="post-title">
-                                <a href="MainpageTekn(1).html">Telegram Ajak Pengguna WhatsApp Hapus Aplikasi</a>
+                                <a href="MainpageTekn(1).php">Telegram Ajak Pengguna WhatsApp Hapus Aplikasi</a>
                             </div>
                         </div>
 
@@ -207,7 +207,7 @@
                                 </div>
                             </div>
                             <div class="post-title">
-                                <a href="MainpageTekn(2).html">Wisata Luar Angkasa Gratis lewat Google Chrome, Begini Caranya</a>
+                                <a href="MainpageTekn(2).php">Wisata Luar Angkasa Gratis lewat Google Chrome, Begini Caranya</a>
                             </div>
                         </div>
                         <div class="post-content" data-aos="flip-up" data-aos-delay="400">
@@ -222,7 +222,7 @@
                                 </div>
                             </div>
                             <div class="post-title">
-                                <a href="MainpageTekn(3).html">Gojek dan Tokopedia Resmi Merger Menjadi GoTo</a>
+                                <a href="MainpageTekn(3).php">Gojek dan Tokopedia Resmi Merger Menjadi GoTo</a>
                             </div>
                         </div>
 
@@ -238,7 +238,7 @@
                                 </div>
                             </div>
                             <div class="post-title">
-                                <a href="MainpageTekn(4).html">WhatsApp Siapkan "Mode Menghilang", Seperti Apa?</a>
+                                <a href="MainpageTekn(4).php">WhatsApp Siapkan "Mode Menghilang", Seperti Apa?</a>
                             </div>
                         </div>
 
@@ -254,7 +254,7 @@
                                 </div>
                             </div>
                             <div class="post-title">
-                                <a href="MainpageTekn(5).html">Layanan 5G Telkomsel Meluncur Sebentar Lagi?</a>
+                                <a href="MainpageTekn(5).php">Layanan 5G Telkomsel Meluncur Sebentar Lagi?</a>
                             </div>
                         </div>
                     </div>
