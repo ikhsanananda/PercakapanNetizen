4a453e0d54e90b4f98d6bc53f4a3e432fe375aa8
Muhammad Ikhsan Ananda <80374569+ikhsanananda@users.noreply.github.com>
2021-06-14 01:25:25 +0700

    Update MainpageLife(7).php

--- MainpageLife(7).php
+++ MainpageLife(7).php
@@ -28,29 +28,52 @@
 
     <nav class="nav">
         <div class="nav-menu flex-row">
-            <div class="nav-brand">
-                <a href="#"><img src="Homepage/assets/PN full color.png" alt="Logo"></a>
-            </div>
+            <ul class="nav-img">       
+                <a href="index.php">
+                        <img src="PN full color.png" alt="Logo">
+                </a>  
+            </ul>
+
             <div class="toggle-collapse">
                 <div class="toggle-icons">
                     <i class="fas fa-bars"></i>
                 </div>
             </div>
+
             <div>
-                <ul class="nav-items">
+            <ul class="nav-items">
                     <li class="nav-link">
-                        <a href="#">News Update</a>
+                        <a href="index.php">Berita Terkini</a>
                     </li>
                     <li class="nav-link">
-                        <a href="#">Share The Story</a>
+                        <a href="ShareStory.php">Sampaikan Cerita Anda</a>
                     </li>
                     <li class="nav-link">
-                        <a href="#">Contact Us</a>
+                        <a href="contact.php">Yuk Kenalan</a>
+                    </li>
+                    <li class="nav-link">
+                        <?php
+                            session_start(); 
+                            if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true)
+                            {
+                        ?>
+                                <a href="profil2.php">Berita Favorit Anda</a>
+                        <?php
+                            }
+                            else
+                            {
+                        ?>
+                                <a href="SignupLogin.php">Masuk atau Daftar</a>
+                        <?php
+                            }
+                        ?>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
+
+
 
     <!-- ------------x---------------  Navigation --------------------------x------------------- -->
 
@@ -71,32 +94,65 @@
                                 <span><i class="fas fa-user text-gray"></i>&nbsp;&nbsp;Admin</span>
                                 <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 21, 2021</span>
                                 <span>
-                                    <a href="ceklogin.php">
-                                        <button>
-                                                <i class="far fa-heart text-gray"></i>
+                                <form action="<?php $halaman=basename($_SERVER['PHP_SELF']); echo $halaman; ?>" method="POST">                                        <button name="simpan">
+                                                <?php 
+                                                    include("config.php");
+                                                    $page=basename($_SERVER['PHP_SELF']); 
+                                                    if($_SESSION['loggedin']==false)
+                                                    {
+                                                        header('Location:ceklogin.php');
+                                                    }
+                                                    else
+                                                    {
+                                                        $nama_user=$_SESSION['username'];
+                                                        $pgsql=pg_query("SELECT count(nama_halaman) FROM simpan_berita WHERE nama_halaman='$page' AND username='$nama_user'");
+                                            
+                                                        $val = pg_fetch_result($pgsql, 0, 0);
+                                                        if($val=='0')
+                                                        {
+                                                    ?>
+                                                            <i class="far fa-heart text-gray"></i>
+                                                    <?php    
+                                                        }
+                                                        else
+                                                       {
+                                                    ?>
+                                                            <i class="fa fa-heart"></i>
+                                                    <?php    
+                                                        }
+                                                    }
+                                                    ?>                                  
                                             </button>&nbsp;&nbsp;Like
                                             <?php
-                                                include("config.php");
-                                                session_start();
-                                                $nama_user=$_SESSION['username'];
-                                                $page=basename($_SERVER['PHP_SELF']);
-                                                
-                                                $pgsql=pg_query("SELECT count(nama_halaman) FROM simpan_berita WHERE nama_halaman='$page' AND username='$nama_user'");
-                                            
-                                                $val = pg_fetch_result($pgsql, 0, 0);
-                                                if($val=='0')
+                                                if(isset($_POST['simpan']))
                                                 {
-                                                    $query=pg_query("INSERT INTO simpan_berita(username, nama_halaman) VALUEs('$nama_user', '$page')");
+                                                    include("config.php");
+                                                
+                                                    if($_SESSION['loggedin']==false)
+                                                    {
+                                                        header('Location:ceklogin.php');
+                                                    }
+                                                    else
+                                                    {
+                                                        $nama_user=$_SESSION['username'];
+                                                
+                                                        $pgsql=pg_query("SELECT count(nama_halaman) FROM simpan_berita WHERE nama_halaman='$page' AND username='$nama_user'");
+                                            
+                                                        $val = pg_fetch_result($pgsql, 0, 0);
+                                                        if($val=='0')
+                                                        {
+                                                            $query=pg_query("INSERT INTO simpan_berita(username, nama_halaman) VALUEs('$nama_user', '$page')");
+                                                        }
+                                                    }
                                                 }
                                             ?>
-                                    </a>
+                                    </form>
                                 </span>
-
                             </div>
                         </div>
 
                         <div class="post-title">
-                            <a href="MainpageLife(7).php">Arti Nama Cicit Ratu Elizabeth, Sarat Makna dan Kenangan</a>
+                        <a href="MainpageLife(7).php">Arti Nama Cicit Ratu Elizabeth, Sarat Makna dan Kenangan</a>
                             <p>Para cicit Ratu Elizabeth, termasuk Lilibet Diana yang baru lahir, memiliki nama yang penuh makna dan kenangan.</p>
                             <p>Pemilihan nama untuk cucu pimpinan monarki Inggris ini cenderung klasik dan sarat tradisi. Kebanyakan menggunakan nama yang pernah dipakai oleh anggota kerajaan Inggris di masa sebelumnya.</p>
                             <p>Meski nama yang dipilih terkesan itu-itu saja, sebenarnya ada makna mendalam untuk setiap nama yang disandang keturunan Kerajaan Inggris ini. Tak heran publik Inggris selalu menantikan nama yang dipilih untuk anggota baru royal family.</p>
@@ -123,38 +179,37 @@
                             <p>Calon pewaris tahta Kerajaan Inggris ini dinamai sesuai nama kakek buyut dari ayahnya, Raja George VI. Sedangkan nama tengahnya terinspirasi dari nama nenek buyutnya, Ratu Elizabeth Alexandra Mary.</p>
                             <p>Sedangkan nama belakangnya dipakai bersama orang-orang terdekatnya termasuk ayahnya, Pangeran William dan adik laki-lakinya, Pangeran Louise.</p>
                             <p>Adapun, George sendiri berarti 'petani' dan Alexandra bermakna 'pembela rakyat', keduanya dalam Bahasa Yunani.</p>
-                        </div>
+                       </div>
                     </div>
                 </div>
 
                 <aside class="sidebar">
-                    <div class="category">
-                        <h2>Category</h2>
+                <div class="category">
+                <h2>Kategori Berita</h2>
                         <ul class="category-list">
                             <li class="list-items" data-aos="fade-left" data-aos-delay="100">
-                                <a href="#">Health</a>
-
+                                <a href="indexEdu.php">Edukasi</a>
                             </li>
+
                             <li class="list-items" data-aos="fade-left" data-aos-delay="200">
-                                <a href="#">Technology</a>
-
+                                <a href="indexFood.php">Makanan</a>
                             </li>
+
                             <li class="list-items" data-aos="fade-left" data-aos-delay="300">
-                                <a href="#">Lifestyle</a>
-
+                                <a href="indexKes.php">Kesehatan</a>
                             </li>
+
                             <li class="list-items" data-aos="fade-left" data-aos-delay="400">
-                                <a href="#">Education</a>
-
+                                <a href="indexTekn.php">Teknologi</a>
                             </li>
-                            <li class="list-items" data-aos="fade-left" data-aos-delay="500">
-                                <a href="#">Food</a>
 
+                            <li class="list-items" data-aos="fade-left" data-aos-delay="500">
+                                <a href="indexLife.php">Gaya Hidup</a>
                             </li>
                         </ul>
                     </div>
                     <div class="popular-post">
-                        <h2>Popular Post</h2>
+                        <h2>Berita Terpopular</h2>
                         <div class="post-content" data-aos="flip-up" data-aos-delay="200">
                             <div class="post-image">
                                 <div>
@@ -165,7 +220,6 @@
                                         <i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 21,
                                         2021
                                     </span>
-                                    <span><button><a href="ceklogin.php"><i class="far fa-heart text-gray"></i></a></button>&nbsp;&nbsp;Like</span>
                                 </div>
                             </div>
 
@@ -182,7 +236,6 @@
                                 <div class="post-info flex-row">
                                     <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 21,
                                         2021</span>
-                                    <span><button><a href="ceklogin.php"><i class="far fa-heart text-gray"></i></a></button>&nbsp;&nbsp;Like</span>
                                 </div>
                             </div>
                             <div class="post-title">
@@ -197,7 +250,6 @@
                                 <div class="post-info flex-row">
                                     <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 21,
                                         2021</span>
-                                    <span><button><a href="ceklogin.php"><i class="far fa-heart text-gray"></i></a></button>&nbsp;&nbsp;Like</span>
                                 </div>
                             </div>
                             <div class="post-title">
@@ -213,7 +265,6 @@
                                 <div class="post-info flex-row">
                                     <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 21,
                                         2021</span>
-                                    <span><button><a href="ceklogin.php"><i class="far fa-heart text-gray"></i></a></button>&nbsp;&nbsp;Like</span>
                                 </div>
                             </div>
                             <div class="post-title">
@@ -229,7 +280,6 @@
                                 <div class="post-info flex-row">
                                     <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;May 21,
                                         2021</span>
-                                    <span><button><a href="ceklogin.php"><i class="far fa-heart text-gray"></i></a></button>&nbsp;&nbsp;Like</span>
                                 </div>
                             </div>
                             <div class="post-title">
@@ -237,24 +287,25 @@
                             </div>
                         </div>
                     </div>
+                    
                     <div class="newsletter" data-aos="fade-up" data-aos-delay="300">
-                        <h2>Newsletter</h2>
-                        <div class="form-element">
-                            <input type="text" class="input-element" placeholder="Email">
-                            <button class="btn form-btn">Subscribe</button>
-                        </div>
+                        <h2>Ikuti Surat Kabar Kami</h2>
+                        <form action="email.php" method="POST">
+                            <div class="form-element">
+                                <input type="text" class="input-element" name="email" placeholder="Email">
+                                <button class="btn form-btn" name="submit">Ikuti</button>
+                            </div>
+                        </form>
                     </div>
+
                     <div class="popular-tags">
                         <h2>Popular Tags</h2>
                         <div class="tags flex-row">
-                            <span class="tag" data-aos="flip-up" data-aos-delay="100">Software</span>
-                            <span class="tag" data-aos="flip-up" data-aos-delay="200">technology</span>
-                            <span class="tag" data-aos="flip-up" data-aos-delay="300">travel</span>
-                            <span class="tag" data-aos="flip-up" data-aos-delay="400">illustration</span>
-                            <span class="tag" data-aos="flip-up" data-aos-delay="500">design</span>
-                            <span class="tag" data-aos="flip-up" data-aos-delay="600">lifestyle</span>
-                            <span class="tag" data-aos="flip-up" data-aos-delay="700">love</span>
-                            <span class="tag" data-aos="flip-up" data-aos-delay="800">project</span>
+                            <span class="tag" data-aos="flip-up" data-aos-delay="100">Makanan</span>
+                            <span class="tag" data-aos="flip-up" data-aos-delay="200">Kesehatan</span>
+                            <span class="tag" data-aos="flip-up" data-aos-delay="300">Teknologi</span>
+                            <span class="tag" data-aos="flip-up" data-aos-delay="400">Edukasi</span>
+                            <span class="tag" data-aos="flip-up" data-aos-delay="500">Gaya Hidup</span>
                         </div>
                     </div>
                 </aside>
@@ -275,16 +326,10 @@
             <div class="about-us" data-aos="fade-right" data-aos-delay="200">
                 <h2>About us</h2>
                 <p>Asmi Devi Azizah G64190009</p>
-                <p>Muhammad Ikhsan Ananda G64190032</p>
-                <p>Ramadhanti Nisa Permanahadi G64190092</p>
+                <p>Muhammad Ikhsan Ananda</p>
+                <p>Ramadhanti Nisa Permanahadi</p>
             </div>
-            <div class="newsletter" data-aos="fade-right" data-aos-delay="200">
-                <h2>Newsletter</h2>
-                <p>Stay update with our latest</p>
-                <div class="form-element">
-                    <input type="text" placeholder="Email"><span><i class="fas fa-chevron-right"></i></span>
-                </div>
-            </div>
+            
         </div>
         <div class="rights flex-row">
             <h4 class="text-gray">
