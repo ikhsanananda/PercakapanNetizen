08eda3f61ddfa35b929c7a424b2025d940113f3a
Muhammad Ikhsan Ananda <80374569+ikhsanananda@users.noreply.github.com>
2021-06-14 01:40:58 +0700

    Update ceklogin.php

--- ceklogin.php
+++ ceklogin.php
@@ -4,15 +4,15 @@ include("config.php");
 session_start();
 if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true)
 {
-    if(time()-$_SESSION["login_time_stamp"] >600)  
+    if(time()-$_SESSION["login_time_stamp"] >6000)  
     {
         session_unset();
-        session_destroy();
+        session_destroy();       
         header('Location:SignupLogin.php');
     }
     else 
     {
-        header('Location:FavouriteNews.html');
+        header('Location:profil2.php');
     }
 }
 else
