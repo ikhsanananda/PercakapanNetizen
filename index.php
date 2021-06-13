cc56245d5e6418c81f8bab5b0a7a5544569e8e37
Muhammad Ikhsan Ananda <80374569+ikhsanananda@users.noreply.github.com>
2021-06-14 01:43:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -38,9 +38,11 @@
 
     <nav class="nav">
         <div class="nav-menu flex-row">
-            <div class="nav-brand">
-                <a href="#"><img src="Homepage/assets/PN full color.png" alt="Logo"></a>
-            </div>
+            <ul class="nav-img">       
+                <a href="index.php">
+                        <img src="PN full color.png" alt="Logo">
+                </a>  
+            </ul>
 
             <div class="toggle-collapse">
                 <div class="toggle-icons">
@@ -57,7 +59,24 @@
                         <a href="ShareStory.php">Sampaikan Cerita Anda</a>
                     </li>
                     <li class="nav-link">
-                        <a href="#">Yuk Kenalan</a>
+                        <a href="contact.php">Yuk Kenalan</a>
+                    </li>
+                    <li class="nav-link">
+                        <?php
+                            session_start(); 
+                            if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true)
+                            {
+                        ?>
+                                <a href="profil2.php">Berita Favorit Anda</a>
+                        <?php
+                            }
+                            else
+                            {
+                        ?>
+                                <a href="SignupLogin.php">Masuk atau Daftar</a>
+                        <?php
+                            }
+                        ?>
                     </li>
                 </ul>
             </div>
@@ -74,7 +93,7 @@
             <div class="site-background" data-aos="fade-up" data-aos-delay="100">
                 <h3>Berita Terkini</h3>
                 <h1>Menginspirasi Melalui Informasi</h1>
-                <a href="SignupLogin.php"><button class="btn">Berita Favorit Anda</button></a>
+                <a href="ceklogin.php"><button class="btn">Berita Favorit Anda</button></a>
             </div>
         </section>
 
@@ -142,7 +161,6 @@
                             <div class="post-info flex-row">
                                 <span><i class="fas fa-user text-gray"></i>&nbsp;&nbsp;Admin</span>
                                 <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;January 14, 2019</span>
-                        
                             </div>
                         </div>
                         <div class="post-title">
@@ -226,23 +244,23 @@
                         <h2>Kategori Berita</h2>
                         <ul class="category-list">
                             <li class="list-items" data-aos="fade-left" data-aos-delay="100">
-                                <a href="edukasi.php">Edukasi</a>
+                                <a href="indexEdu.php">Edukasi</a>
                             </li>
 
                             <li class="list-items" data-aos="fade-left" data-aos-delay="200">
-                                <a href="makanan.php">Makanan</a>
+                                <a href="indexFood.php">Makanan</a>
                             </li>
 
                             <li class="list-items" data-aos="fade-left" data-aos-delay="300">
-                                <a href="kesehatan.php">Kesehatan</a>
+                                <a href="indexKes.php">Kesehatan</a>
                             </li>
 
                             <li class="list-items" data-aos="fade-left" data-aos-delay="400">
-                                <a href="teknologi.php">Teknologi</a>
+                                <a href="indexTekn.php">Teknologi</a>
                             </li>
 
                             <li class="list-items" data-aos="fade-left" data-aos-delay="500">
-                                <a href="gayahidup.php">Gaya Hidup</a>
+                                <a href="indexLife.php">Gaya Hidup</a>
                             </li>
                         </ul>
                     </div>
@@ -272,8 +290,8 @@
                                 </div>
                                 <div class="post-info flex-row">
                                     <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;January 14,
-                                        2019</span>
-                                        
+                                        2019
+                                    </span>
                                 </div>
                             </div>
                             <div class="post-title">
